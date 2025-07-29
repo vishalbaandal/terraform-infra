cdc7d63e049152d4134e1791dda401380c91777d
vishalbaandal <123541986+vishalbaandal@users.noreply.github.com>
2025-07-29 23:32:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "elb_dns_name" {
-  value = aws_lb.app_lb.dns_name
+  value       = aws_lb.app_lb.dns_name
   description = "Load Balancer DNS"
 }
 
 output "s3_bucket_name" {
-  value       = aws_s3_bucket.project_bucket[0].bucket
+  value       = var.enable_s3_bucket ? aws_s3_bucket.project_bucket[0].bucket : null
   description = "Optional S3 bucket name"
-  condition   = var.enable_s3_bucket
 }
